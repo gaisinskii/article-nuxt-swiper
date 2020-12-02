69f24a5e2f9c837e288588ce73a79a1b409961c5
Gaisinskii Andrey <a.gaisinskii@admitad.pro>
2020-12-02 18:16:03 +0300

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
+  router: {
+    base: '/article-nuxt-swiper/'
+  },
+
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'article-nuxt-swiper',
